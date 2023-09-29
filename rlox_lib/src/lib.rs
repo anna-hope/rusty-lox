373baac06346aa0a7176bd649fbafe9fbcead264
Anna Melnikov <anna.me@hey.com>
2023-09-29 03:54:40 -0500

    feat: use mimalloc
    Use MiMalloc instead of the default allocator
    for increased performance.

--- rlox_lib/src/lib.rs
+++ rlox_lib/src/lib.rs
@@ -1,8 +1,13 @@
+use mimalloc::MiMalloc;
+
 mod chunk;
 mod compiler;
 mod scanner;
 mod value;
 pub mod vm;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 #[cfg(test)]
 mod tests {}
